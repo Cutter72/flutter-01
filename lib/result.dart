2af672b06dcec547e2f26be632d386e9d00e6bbe
Paweł Drelich <drelich_pawel@o2.pl>
2022-10-25 23:07:16 +0200

    Day 3 Result text style added

--- lib/result.dart
+++ lib/result.dart
@@ -8,7 +8,14 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Your score is $_totalScore!"),
+      child: Text(
+        "Your score is $_totalScore!",
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
